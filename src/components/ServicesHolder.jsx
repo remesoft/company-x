cad78ddaa4345d470ed5864f082e21b856a36362
remesoft <email.dwip@gmail.com>
2024-11-14 11:37:21 -0800

    slider added

--- src/components/ServicesHolder.jsx
+++ src/components/ServicesHolder.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ServiceCard from "./ui/ServiceCard";
 import { Link } from "react-router-dom";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
-export default function ServicesHolder({ title }) {
+export default function ServicesHolder({ title, index }) {
+  const splideRef = useRef(null);
+
+  const options = {
+    perPage: 4,
+    gap: "1rem",
+    breakpoints: {
+      1024: {
+        perPage: 3,
+      },
+      768: {
+        perPage: 1,
+      },
+    },
+  };
+
   return (
-    <section className="pt-10 lg:pt-16">
+    <section className="relative pt-10 lg:pt-16">
       <div className="mx-auto max-w-[1250px] px-2">
         <div className="flex items-end justify-between">
           <h2 className="text-xl font-semibold lg:text-2xl">{title}</h2>
@@ -12,12 +29,27 @@ export default function ServicesHolder({ title }) {
             View All Services
           </Link>
         </div>
-        <div className="mt-4 grid grid-cols-2 gap-2 md:grid-cols-3 lg:mt-8 lg:grid-cols-4 lg:gap-6">
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
-        </div>
+        <Splide
+          ref={splideRef}
+          options={options}
+          className={`relative mt-4 lg:mt-8 splide-instance-${index}`} // Unique class for each instance
+        >
+          <SplideSlide>
+            <ServiceCard />
+          </SplideSlide>
+          <SplideSlide>
+            <ServiceCard />
+          </SplideSlide>
+          <SplideSlide>
+            <ServiceCard />
+          </SplideSlide>
+          <SplideSlide>
+            <ServiceCard />
+          </SplideSlide>
+          <SplideSlide>
+            <ServiceCard />
+          </SplideSlide>
+        </Splide>
       </div>
     </section>
   );
