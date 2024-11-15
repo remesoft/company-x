97ae4e0516291bd3248f9863c0004ba37dcb7fc8
Anik <deyanik659@gmail.com>
2024-11-15 16:21:46 +0600

    lg footer done

--- src/components/ui/CollapsibleMenu.jsx
+++ src/components/ui/CollapsibleMenu.jsx
@@ -8,24 +8,45 @@ const CollapsibleMenu = ({ title, menuItems }) => {
   const toggleBtn = () => {
     setIsOpen(!isOpen);
   };
-  return(
-  <details className="my-4">
-    <summary className="flex list-none items-center justify-between gap-2 rounded-md px-4 py-2">
-      <h3 className="text-xl text-slate-200">{title}</h3>
-      <button onClick={toggleBtn} className="font-semibold text-slate-200">
-        {isOpen ? <ArrowUp01Icon size={20} /> : <ArrowDown01Icon size={20} />}
-      </button>
-    </summary>
-    <ul>
-      {menuItems.map((item, index) => (
-        <li key={index} className="border-slate-400">
-          <Link className="block px-4 py-2 text-slate-200 hover:text-blue-600">
-            {item}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </details>);
+
+  return (
+    <>
+      {/* Collapsible menu for small screens */}
+      <details className="my-4 block md:hidden">
+        <summary className="flex list-none items-center justify-between gap-2 rounded-md px-4 py-2">
+          <h3 className="text-xl text-slate-200">{title}</h3>
+          <button onClick={toggleBtn} className="font-semibold text-slate-200">
+            {isOpen ? (
+              <ArrowUp01Icon size={20} />
+            ) : (
+              <ArrowDown01Icon size={20} />
+            )}
+          </button>
+        </summary>
+        <ul>
+          {menuItems.map((item, index) => (
+            <li key={index} className="border-slate-400">
+              <Link className="block px-4 py-2 text-slate-200 hover:text-slate-100">
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </details>
+
+      {/* Expanded list for large screens */}
+      <ul className="my-4 hidden flex-col md:block">
+      <h3 className="text-xl text-slate-200 md:mb-4">{title}</h3>
+        {menuItems.map((item, index) => (
+          <li key={index} className="border-slate-400">
+            <Link className="block px-4 py-2 text-slate-200 hover:text-slate-100">
+              {item}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default CollapsibleMenu;
