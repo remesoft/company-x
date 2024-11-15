76ccafa12b75327e9c21ed7996fdb1fb53fd4b1f
remesoft <email.dwip@gmail.com>
2024-11-14 20:45:54 -0800

    fix ui bugs of banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -21,20 +21,20 @@ function Banner() {
           <p className="my-2 text-white opacity-80 lg:text-lg">
             We are offering 1200+ service for you.
           </p>
-          <form
-            action=""
-            className="mt-2 flex w-[90%] items-center self-start rounded-full bg-white pl-2 lg:w-[65%] lg:pl-4"
-          >
-            <Search01Icon className="h-5 lg:h-6" />
-            <input
-              type="text"
-              placeholder="Search service title..."
-              className="ml-4 w-20 flex-grow bg-transparent focus:outline-none lg:py-2"
-            />
+          <form action="" className="mt-2 flex w-[90%] lg:w-[65%]">
+            <div className="flex flex-grow items-center rounded-l-full bg-white pl-2 lg:pl-4">
+              <Search01Icon className="h-5 lg:h-6" />
+              <input
+                type="text"
+                placeholder="Search service title..."
+                className="ml-4 w-20 flex-grow bg-transparent focus:outline-none lg:py-2"
+              />
+            </div>
+
             <input
               type="submit"
               value="Search"
-              className="h-full rounded-r-full bg-blue-600 px-6 py-2 font-semibold text-white lg:py-4"
+              className="h-full rounded-r-full bg-blue-600 px-6 py-2 font-semibold text-white transition hover:bg-blue-700 lg:py-4"
             />
           </form>
         </div>
