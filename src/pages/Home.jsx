2e7cf299875b4a6c40b9bb29017923ced40d27f4
remesoft <email.dwip@gmail.com>
2024-11-15 23:30:12 -0800

    Fix services bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Banner from "../components/Banner";
 import CategoriesHolder from "../components/CategoriesHolder";
-import ServicesHolder from "../components/ServicesHolder";
-import avatar from "../assets/images/avatar.jpg";
-import TestimonialCard from "../components/ui/TestimonialCard";
 import Testimonial from "../components/Testimonial";
-import Services from "../components/ServiceSlider";
 import ServiceSlider from "../components/ServiceSlider";
 
 function Home() {
