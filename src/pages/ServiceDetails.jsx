7490344885f5a92ab9fb13bb938ee0244ba6ac81
remesoft <email.dwip@gmail.com>
2024-11-17 02:42:50 -0800

    services page done

---
+++ src/pages/ServiceDetails.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ServiceDetails() {
+  return <div>ServiceDetails</div>;
+}
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BannerSecondary from "../components/BannerSecondary";
 import ServiceManager from "../components/ServiceManager";
 
